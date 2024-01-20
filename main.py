07ec5f28b3edb1a56361c7f64e72025a229a0b57
corozzia <gajfullink21@gmail.com>
2024-01-20 15:57:03 +0500

    second

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import sys
 
 import pygame
 
-WIDTH = 1200  # ширина игрового окна
-HEIGHT = 800  # высота игрового окна
-BG_COLOR = (0, 0, 0)
 
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("My Game")
+
+back = pygame.image.load("images/New Piskel.png")
+screen.blit(back, (0, 0))
 def run_game():
-    pygame.init()
-    pygame.mixer.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("My Game")
     while True:
-        # for event in pygame.event.get():
-        #     if event.type == pygame.quit():
-        #         sys.exit()
-        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
 run_game()
