8d98f95883ae2494a8635d519c9091fa40fd6fe7
corozzia <gajfullink21@gmail.com>
2024-01-20 17:26:45 +0500

    third commit

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import sys
-
 import pygame
 
-
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((1200, 800))
-pygame.display.set_caption("My Game")
-
 back = pygame.image.load("images/New Piskel.png")
-screen.blit(back, (0, 0))
+
 def run_game():
+    pygame.init()
+    pygame.mixer.init()
+    screen = pygame.display.set_mode((1000, 800))
+    pygame.display.set_caption("My Game")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+        screen.blit(back, (0, 0))
+        pygame.display.update()
+
 
 run_game()
